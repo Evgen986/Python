91d907e67d6bd7c1bd9f65eb2d29419905668e35
Evgen986 <Evgen986@mail.ru>
2022-12-16 18:10:33 +0300

    added stepic task

--- Stepic/Intersection_of_segments.py
+++ Stepic/Intersection_of_segments.py
@@ -1,11 +1,19 @@
 a1, a2, b1, b2 = int(input()), int(input()), int(input()), int(input())
-if a2==b1:
-    print(a2)
-elif a1==b2:
-    print(a1)
-elif b1>a2:
-    print(b1, a2)
-elif b2>a1:
-    print(a1, b2)
+if a1<b1:
+    if a2==b1:
+        print(a2)
+    elif a2>b1 and (b2>a2 or b2==a2):
+        print(b1, a2)
+    elif a2>b1 and b2<a2:
+        print(b1, b2)
+    else:
+        print('пустое множество')
 else:
-    print('пустое множество')
+    if b2==a1:
+        print(b2)
+    elif b2>a1 and (a2>b2 or b2==a2):
+        print(a1, b2)
+    elif b2>a1 and a2<b2:
+        print(a1, a2)
+    else:
+        print('пустое множество')
