910acae731a59caea1184f2c56b7f86772c85cc9
Evgen986 <Evgen986@mail.ru>
2022-12-26 21:00:29 +0300

    correct task_5

--- GB/Seminars/Seminar_4/Home_Work/Task_5.py
+++ GB/Seminars/Seminar_4/Home_Work/Task_5.py
@@ -1,7 +1,7 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-from Task_4 import write_to_file
+import Task_4
 
 
 def read_file(name_file):  # Функция чтения из файла
@@ -22,24 +22,25 @@ def sum_polynomials(poly_1, poly_2):  # Функция сложения мног
     for index in range(0, length_list, 2):
         new_poly += polynomials_1[index] + ' + '
     for index in range(0, len(polynomials_2) - 2, 2):
-        temp_1 = polynomials_1[index + length_list]
-        temp_2 = polynomials_2[index]
-        index_1 = temp_1.find('x')
-        index_2 = temp_2.find('x')
+        index_1 = polynomials_1[index + length_list].find('x')
+        index_2 = polynomials_2[index].find('x')
         if index_1 < 0 or index_2 < 0:
-            new_poly += str(int(temp_1) + int(temp_2)) + ' = 0 '
+            new_poly += str(int(polynomials_1[index + length_list]) + int(polynomials_2[index])) + ' = 0 '
         else:
-            new_poly += str(int(temp_1[:index_1]) + int(temp_2[:index_2])) + temp_1[index_1:] + ' + '
+            new_poly += str(int(polynomials_1[index + length_list][:index_1]) + int(polynomials_2[index][:index_2])) + \
+                        polynomials_1[index + length_list][index_1:] + ' + '
     return new_poly
 
 
 def main():
+    Task_4.main()
     p_1 = read_file('polynomial_1.txt')
     p_2 = read_file('polynomial_2.txt')
     print(f'многочлен №1 = {p_1}')
     print(f'многочлен №2 = {p_2}')
     print(f'сумма многочленов = {sum_polynomials(p_1, p_2)}')
-    write_to_file('sum_polynomials.txt', f'сумма многочленов {p_1} и {p_2} равняется {sum_polynomials(p_1, p_2)}')
+    Task_4.write_to_file('sum_polynomials.txt',
+                         f'сумма многочленов {p_1} и {p_2} \nравняется {sum_polynomials(p_1, p_2)}')
 
 
 if __name__ == '__main__':
