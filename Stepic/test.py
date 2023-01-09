e79ca2ed6b2a5b8bb918301464f25a9cbb7d0376
Evgenyi_Malyutin <Evgen986@mail.run>
2023-01-09 13:13:19 +0300

    merge

--- Stepic/test.py
+++ Stepic/test.py
@@ -1,2 +1,13 @@
-for i in range(int(input()), int(input())+1):
-    print(chr(i), end=' ')
+my_list = list()
+for i in range(int(input())):
+    my_list.append(input())
+search_list = list()
+for j in range(int(input())):
+    search_list.append(input())
+for index in range(len(my_list)):
+    count = 0
+    for ind in range(len(search_list)):
+        if search_list[ind].lower() in my_list[index].lower():
+            count += 1
+    if count == len(search_list):
+        print(my_list[index])
