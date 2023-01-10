2dac3ff330a1768ae4afd3f8ba51e8042d0cddda
Evgen986 <Evgen986@mail.ru>
2023-01-10 17:03:26 +0300

    temp

--- Stepic/test.py
+++ Stepic/test.py
@@ -1,7 +1,5 @@
-import math
-
-n = math.log2(float(input()))
-index = str(n).find('.')
-if int(str(n)[index + 1:]) != 0:
-    n = int(n) + 1
-print(int(n))
+numbers = [-6, -8, 0, 1, 3, 8, -7, 12, 17, 24, 25, 3, 5, 1]
+res = 0
+for num in numbers:
+    res += (num % 2 == 1) and (num > 1)
+print(res)
