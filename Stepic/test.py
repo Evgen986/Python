75f2d265fb53645268712f71b57473d00366c280
Evgen986 <Evgen986@mail.ru>
2023-01-11 16:54:10 +0300

    added tasks stepic

--- Stepic/test.py
+++ Stepic/test.py
@@ -1,13 +1,2 @@
-#  g i v e t h h i i s m a a a n a g u u n
-text = input().replace(' ', '')
-count = int(input())
-result_list = list()
-while len(text) != 0:
-    new_list = list()
-    if count > len(text):
-        count = len(text)
-    for index in range(count):
-        new_list.append(text[index])
-    text = text[count:]
-    result_list.append(new_list)
-print(result_list)
+l = {25, 87, 95}
+print(len(l))
