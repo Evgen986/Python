fee7d617b7b9d0c06dc1b6d189e07588d45ed4b1
Evgenyi_Malyutin <Evgen986@mail.run>
2023-01-13 10:37:02 +0300

    temp

--- Stepic/test.py
+++ Stepic/test.py
@@ -1,13 +1,7 @@
-#  g i v e t h h i i s m a a a n a g u u n
-text = input().replace(' ', '')
-count = int(input())
-result_list = list()
-while len(text) != 0:
-    new_list = list()
-    if count > len(text):
-        count = len(text)
-    for index in range(count):
-        new_list.append(text[index])
-    text = text[count:]
-    result_list.append(new_list)
-print(result_list)
+matrix = [[int(el) for el in input().split()] for rows in range(int(input()))]
+maximal = matrix[0][0]
+for rows in range(len(matrix)):
+    for coloms in range(rows+1):
+        if matrix[rows][coloms] > maximal:
+            maximal = matrix[rows][coloms]
+print(maximal)
--- Stepic/Подсписки списка.py
+++ Stepic/Подсписки списка.py
@@ -4,6 +4,8 @@ for step in range(1, len(text)+1):
     new_list = list()
     for start_index in range(len(text)):
         if len(text)-start_index >= step:
-            new_list.append(text[start_index:start_index+step])
+            new_list.append(x for x in text[start_index:start_index+step])
     result_list.append(new_list)
 print(result_list)
+
+# [[], ['1', '2', '3', '0'], ['12', '23', '30'], ['123', '230'], ['1230']]
